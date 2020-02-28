3d83066628ff530217d7030eaa0b75230499dfbe
Gary Wheeler <gkwheeler40@gmail.com>
2020-02-28 17:46:45 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -56,7 +56,11 @@ class Student
   end
   
   def self.first_X_students_in_grade_10(x)
+    sql = <<-SQL
+    SELECT *
+    FROM students 
     
+    SQL
   end
   
   def self.first_student_in_grade_10
