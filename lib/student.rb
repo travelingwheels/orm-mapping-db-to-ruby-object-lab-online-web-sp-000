80024d0ae7ab1cf2ea29e34355293e6ef4f0a7eb
Gary Wheeler <gkwheeler40@gmail.com>
2020-02-28 18:05:28 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -57,13 +57,13 @@ class Student
   
   def self.first_X_students_in_grade_10(x)
     sql = <<-SQL
-       SELECT * FROM students WHERE grade = 10 LIMIT ?
+       SELECT * 
+       FROM students
+       WHERE grade = 10 
+       LIMIT ?
     SQL
 
-    DB[:conn].execute(sql, x).map do |row|
-      self.new_from_db(row)
-    end.first
-    #binding.pry
+    DB[:conn].execute(sql, x)
   end
 
   #def self.first_student_in_grade_10
