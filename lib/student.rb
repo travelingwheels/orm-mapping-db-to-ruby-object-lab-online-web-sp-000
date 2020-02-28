c0228da76427cbb31c43ffbf78bc52a86fb08886
Gary Wheeler <gkwheeler40@gmail.com>
2020-02-28 17:32:55 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -45,13 +45,8 @@ class Student
   end
   
   def self.students_below_12th_grade
-    sql = <<-SQL
-      SELECT * 
-      FROM students 
-      WHERE grade < 12
-     SQL
-     
-      DB[:conn].execute(sql)
+    sql = "SELECT * FROM students WHERE grade < 12"
+    DB[:conn].execute(sql)
   end
   
   def save
