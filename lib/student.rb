b8992a6750a139b12eae6b746d28e36b57f9f0b8
Gary Wheeler <gkwheeler40@gmail.com>
2020-02-28 18:14:02 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -59,10 +59,12 @@ class Student
     sql = <<-SQL
        SELECT * 
        FROM students
-       WHERE grade = 10 
+       WHERE students.grade = 10 
        LIMIT ?
     SQL
-    DB[:conn].execute(sql, x)
+    DB[:conn].execute(sql, x).collect do |row|
+      self.new_from_db(row)
+    end.first
   end
 
   def save
